072a3c47ad7e4ef0df32eb919857eacb39139c62
lufuno <lufunodagada43@gmail.com>
2025-01-21 13:45:39 +0200

    added weather time and date

--- weather.js
+++ weather.js
@@ -1,8 +1,17 @@
 function refreshweather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#cityname");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElememt = document.querySelector("#Humidity");
+  let windElement = document.querySelector("#windspeed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElememt.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed} km/h`;
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 }
 
@@ -18,6 +27,23 @@ function InputSerach(event) {
 
   //add API//
   searchCity(input.value);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 let searchForm = document.querySelector("#search-form");
